fc209b9b20dc3b71d60191664d36e5e0366436d0
Stjepan <ssalopek.hr@gmail.com>
2021-09-28 13:29:57 +0200

    Optimizing lines, making average of left/right lines

--- lanes.py
+++ lanes.py
@@ -16,10 +16,7 @@ def canny(image):
 def display_lines(image, lines):
     line_image = np.zeros_like(image) # Black image
     if lines is not None:
-        for line in lines:
-            # Right now each line is 2D array [x1,y1,x2,y2]
-            # Reshape it into 1D array with 4 elements
-            x1, y1, x2, y2 = line.reshape(4)
+        for x1, y1, x2, y2 in lines:
             # Draw lines
             # cv2.line(input image, start point, end point, color, thickness)
             cv2.line(line_image, (x1, y1), (x2, y2), (255,0,0), 5)
@@ -43,16 +40,45 @@ def region_of_interest(image):
     masked_image = cv2.bitwise_and(image, mask)
     return masked_image
 
+def make_coordinates(image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1*(3/5))
+    x1 = int((y1 - intercept) / slope)
+    x2 = int((y2 - intercept) / slope)
+    return np.array([x1, y1, x2, y2])
+
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    left_line = make_coordinates(image, left_fit_average)
+    right_line = make_coordinates(image, right_fit_average)
+    return np.array([left_line, right_line])
+
 # Load image, make copy of it
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 
-canny = canny(lane_image)
-cropped_image = region_of_interest(canny)
+canny_image = canny(lane_image)
+cropped_image = region_of_interest(canny_image)
 # https://bit.ly/2XQgX7I
 # cv2.HoughLinesP(input image, rho, theta, treshold, placeholder array, min length of line, max allowed gap btw lines)
 lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
-line_image = display_lines(lane_image, lines)
+
+averaged_lines = average_slope_intercept(lane_image, lines)
+
+line_image = display_lines(lane_image, averaged_lines)
 
 # Blend image to another with defined weights(transparency, translucency)
 # https://bit.ly/3iy2gxN
